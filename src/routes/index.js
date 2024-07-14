79282190842bbc4eb2f4a3d7772acab46eedae0e
tharlles-anjos <luiz_anjos22@hotmail.com>
2024-07-14 01:26:10 -0300

    Foi adicionado a listagem de jogadores cadastrados

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,10 @@ const { Player } = require('../server'); // Importa o modelo Player do server.js
 
 // Rota para adicionar um jogador
 router.post('/adicionar-jogador', async (req, res) => {
-    const { name } = req.body;
+    const { name, age, position } = req.body;
 
     try {
-        const novoJogador = await Player.create({ name });
+        const novoJogador = await Player.create({ name, age, position });
         res.status(201).json(novoJogador);
     } catch (err) {
         console.error('Erro ao adicionar jogador:', err);
@@ -17,5 +17,17 @@ router.post('/adicionar-jogador', async (req, res) => {
     }
 });
 
+// Rota para listar todos os jogadores
+router.get('/listar-jogadores', async (req, res) => {
+    try {
+        const jogadores = await Player.findAll();
+        res.status(200).json(jogadores);
+    } catch (err) {
+        console.error('Erro ao buscar jogadores:', err);
+        res.status(500).json({ error: 'Erro ao buscar jogadores.' });
+    }
+});
+
 module.exports = router;
+
 
